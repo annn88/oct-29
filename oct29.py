755b2b7174bcbcbc4510566353bc00a815c7a6ef
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-10-29 14:38:21 +0530

    Update oct29.py

--- oct29.py
+++ oct29.py
@@ -1,1 +1,10 @@
-
+number =int(input("enter the number:"))
+isprime =True
+for i in range(2,number//2+1):
+    if number % i ==0:
+        isprime =False
+        break
+if isprime:
+    print(f"the given number{number} is prime")
+else:
+    print(f"the given number {number} is not prime")
